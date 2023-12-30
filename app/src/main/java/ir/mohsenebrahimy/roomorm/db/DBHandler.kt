aa3dfd6fce58b903545dc73c49cb22bd47b6b6ac
mohsenebrahimyir <mohsenebrahimy.ir@mail.com>
2023-12-30 21:15:35 +0330

    add write database class with Room ORM

--- app/src/main/java/ir/mohsenebrahimy/roomorm/db/DBHandler.kt
+++ app/src/main/java/ir/mohsenebrahimy/roomorm/db/DBHandler.kt
@@ -1,7 +1,21 @@
 package ir.mohsenebrahimy.roomorm.db
 
-class DBHandler {
+import androidx.room.Database
+import ir.mohsenebrahimy.roomorm.db.dao.UserDAO
+import ir.mohsenebrahimy.roomorm.db.model.UserEntity
+
+@Database(
+    entities = [UserEntity::class],
+    version = DBHandler.DATABASE_VERSION
+)
+abstract class DBHandler {
+
+    abstract fun userDao(): UserDAO
+
     companion object {
+        const val DATABASE_NAME = "main_database"
+        const val DATABASE_VERSION = 1
+
         const val USER_TABLE = "userTable"
     }
 }
