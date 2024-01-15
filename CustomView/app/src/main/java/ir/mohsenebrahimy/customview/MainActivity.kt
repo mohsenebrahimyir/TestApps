5d438c3d8c87966bb13461f75cba84ffe54b6306
mohsenebrahimyir <mohsenebrahimy.ir@mail.com>
2024-01-15 18:38:42 +0330

    add animation (1, 2, 3) from chapter 17

--- CustomView/app/src/main/java/ir/mohsenebrahimy/customview/MainActivity.kt
+++ CustomView/app/src/main/java/ir/mohsenebrahimy/customview/MainActivity.kt
@@ -2,13 +2,60 @@ package ir.mohsenebrahimy.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import ir.mohsenebrahimy.customview.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.lottieSkate.setAnimationFromUrl("https://www.test.com/loading.json")
+
+        binding.seekBar.setOnSeekBarChangeListener (
+            object : OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    binding.lottieLoading.progress = progress.toFloat() / 100
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+            }
+        )
+
+        binding.btnPlay.setOnClickListener {
+            binding.lottieLoading.playAnimation()
+            binding.lottieLoading.loop(true)
+        }
+
+        binding.btnStop.setOnClickListener {
+            binding.lottieLoading.loop(false)
+            binding.lottieLoading.pauseAnimation()
+        }
+
+        loading()
+
+    }
+
+    private var counter = 1
+
+    private fun loading() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.seekBar.progress = counter++
+            loading()
+        }, 100)
     }
 }
